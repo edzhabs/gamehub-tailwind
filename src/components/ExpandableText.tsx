abde89b172ff75ff9fca491d5e7465bbd26ae7da
edzhabs <edzhabs@gmail.com>
2023-09-25 22:55:46 +0800

    Build expandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+interface Props {
+  children: string;
+}
+
+const ExpandableText = ({ children }: Props) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const limit = 300;
+
+  if (!children) null;
+  if (children.length < 300) return <p>{children}</p>;
+  const summary = expanded ? children : children.substring(0, limit) + "...";
+  return (
+    <p className="leading-7">
+      {summary}{" "}
+      <button
+        className="ml-1 px-2 py-1 rounded-md bg-yellow-300 text-black font-semibold text-sm"
+        onClick={() => setExpanded(!expanded)}
+      >
+        {expanded ? "Show Less" : "Read More"}
+      </button>
+    </p>
+  );
+};
+
+export default ExpandableText;
